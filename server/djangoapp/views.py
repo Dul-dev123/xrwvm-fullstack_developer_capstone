29beaca8536747caafd5148a3c907254660b8a36
Dul-dev123 <devminspduli17@gmail.com>
2026-01-04 10:03:29 -0500

    Add all final server files including environment variables

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,70 +1,53 @@
-from django.http import JsonResponse
-from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
-import logging
-import json
+from django.contrib.auth import logout, login, authenticate
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-
-# New imports for Car models and populate script
-from .models import CarMake, CarModel
-from .populate import initiate
+import json
+import logging
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 logger = logging.getLogger(__name__)
 
 @csrf_exempt
 def login_user(request):
     data = json.loads(request.body)
-    username = data['userName']
+    username = data['username']
     password = data['password']
     user = authenticate(username=username, password=password)
-    data = {"userName": username}
     if user is not None:
         login(request, user)
-        data = {"userName": username, "status": "Authenticated"}
-    return JsonResponse(data)
+        return JsonResponse({"userName": username, "status": "Authenticated"})
+    return JsonResponse({"userName": username, "status": "Failed"})
 
-def logout_request(request):
-    logout(request)
-    data = {"userName": ""}
-    return JsonResponse(data)
+def get_dealerships(request, state="All"):
+    endpoint = "/fetchDealers" if state == "All" else "/fetchDealers/"+state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status": 200, "dealers": dealerships})
+
+def get_dealer_reviews(request, dealer_id):
+    if(dealer_id):
+        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+        reviews = get_request(endpoint)
+        for review_detail in reviews:
+            response = analyze_review_sentiments(review_detail['review'])
+            review_detail['sentiment'] = response['sentiment']
+        return JsonResponse({"status": 200, "reviews": reviews})
+    return JsonResponse({"status": 400, "message": "Bad Request"})
+
+def get_dealer_details(request, dealer_id):
+    if(dealer_id):
+        endpoint = "/fetchDealer/"+str(dealer_id)
+        dealership = get_request(endpoint)
+        return JsonResponse({"status": 200, "dealer": dealership})
+    return JsonResponse({"status": 400, "message": "Bad Request"})
 
 @csrf_exempt
-def registration(request):
-    data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
-    first_name = data['firstName']
-    last_name = data['lastName']
-    email = data['email']
-    
-    username_exist = False
-    try:
-        User.objects.get(username=username)
-        username_exist = True
-    except:
-        logger.debug("{} is new user".format(username))
-
-    if not username_exist:
-        user = User.objects.create_user(
-            username=username, 
-            first_name=first_name, 
-            last_name=last_name, 
-            password=password, 
-            email=email
-        )
-        login(request, user)
-        return JsonResponse({"userName": username, "status": "Authenticated"})
-    else:
-        return JsonResponse({"userName": username, "error": "Already Registered"})
-
-# New view to handle fetching cars and auto-populating if empty
-def get_cars(request):
-    count = CarMake.objects.filter().count()
-    print(count)
-    if(count == 0):
-        initiate()
-    car_models = CarModel.objects.select_related('car_make')
-    cars = []
-    for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
-    return JsonResponse({"CarModels":cars})
+def add_review(request):
+    if not request.user.is_anonymous:
+        try:
+            data = json.loads(request.body)
+            post_review(data)
+            return JsonResponse({"status": 200})
+        except:
+            return JsonResponse({"status": 401, "message": "Error in posting review"})
+    return JsonResponse({"status": 403, "message": "Unauthorized"})
